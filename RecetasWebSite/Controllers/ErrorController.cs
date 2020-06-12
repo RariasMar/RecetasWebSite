5924ff2211141e1feb0c2cc3180d1e087666a468
Rubén Arias <rubenarias@outlook.com>
2020-06-12 16:05:32 +0200

    Added comments

--- RecetasWebSite/Controllers/ErrorController.cs
+++ RecetasWebSite/Controllers/ErrorController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RecetasWebSite.Models;
@@ -7,15 +6,26 @@ using System.Diagnostics;
 
 namespace RecetasWebSite.Controllers
 {
+    /// <summary>
+    /// Controlador para gestionar los errores ocurridos en la aplicación
+    /// </summary>
     public class ErrorController : Controller
     {
         private readonly ILogger<ErrorController> _logger;
 
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
+        /// <param name="logger"></param>
         public ErrorController(ILogger<ErrorController> logger)
         {
             _logger = logger;
         } 
 
+        /// <summary>
+        /// Redirige a la pantalla de error cuando ocurre alguno
+        /// </summary>
+        /// <returns>LA vista error</returns>
         [Route("Error")]
         [AllowAnonymous]
         public IActionResult Error()
--- RecetasWebSite/Models/ErrorViewModel.cs
+++ RecetasWebSite/Models/ErrorViewModel.cs
@@ -2,10 +2,19 @@ using System;
 
 namespace RecetasWebSite.Models
 {
+    /// <summary>
+    /// Objeto con los datos del error
+    /// </summary>
     public class ErrorViewModel
     {
+        /// <summary>
+        /// Identificador de la petición que falla
+        /// </summary>
         public string RequestId { get; set; }
 
+        /// <summary>
+        /// Booleano para mostrar u ocultar el identificador de la petición
+        /// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
