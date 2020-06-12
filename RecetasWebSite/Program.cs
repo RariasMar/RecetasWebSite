2c1b53f2d81622e1b79e000876bcab5e34cbeb74
Rubén Arias <rubenarias@outlook.com>
2020-06-12 17:03:51 +0200

    a

--- RecetasWebSite/Program.cs
+++ RecetasWebSite/Program.cs
@@ -5,6 +5,10 @@ namespace RecetasWebSite
 {
     public class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
